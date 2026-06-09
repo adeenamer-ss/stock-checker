4e3346846fa4b14f7f41dbdc2cd6fc80b794e7da
adeenamer-ss <ss.adeenamer@gmail.com>
2026-06-09 17:21:17 +0500

    Add files via upload

---
+++ pages/stock_result.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+from pathlib import Path
+from stock_reporter import stock_data
+
+
+st.title("Stock File", text_alignment="center")
+search_product = st.text_input("Enter product")
+
+search_df = stock_data[stock_data['Product'].str.contains(search_product, case=False, na=False)]
+st.write(search_df)
