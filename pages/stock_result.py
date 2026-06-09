6b5601b5517cab555e8f1ac9a996b1a57397831e
adeenamer-ss <ss.adeenamer@gmail.com>
2026-06-09 17:28:56 +0500

    Update stock_result.py

--- pages/stock_result.py
+++ pages/stock_result.py
@@ -6,5 +6,5 @@ from streamlit_app import stock_data
 st.title("Stock File", text_alignment="center")
 search_product = st.text_input("Enter product")
 
-search_df = stock_data[stock_data['Product'].str.contains(search_product, case=False, na=False)]
+search_df = stock_data[stock_data.index.str.contains(search_product, case=False, na=False)]
 st.write(search_df)
